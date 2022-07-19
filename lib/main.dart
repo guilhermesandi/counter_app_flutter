607cb43336186748b70fb804333624b3f21f98d8
Guilherme Sandi <guilherme_sandi@hotmail.com>
2022-07-19 19:10:17 -0300

    Create counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meu Primeiro App"),
+      ),
+      body: Center(
+          child: Text(
+        "Contador\n$count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
